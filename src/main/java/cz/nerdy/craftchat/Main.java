f350b2074688de5917792236e29b74fcb1cbd685
MrWakeCZ <joseff.kral@gmail.com>
2020-07-22 01:16:26 +0200

    Přidána podpora 1.16.1

--- src/main/java/cz/nerdy/craftchat/Main.java
+++ src/main/java/cz/nerdy/craftchat/Main.java
@@ -8,6 +8,7 @@ import cz.nerdy.craftchat.listeners.PlayerListener;
 import cz.nerdy.craftchat.nms.PluginCompatibility;
 import cz.nerdy.craftchat.nms.Spigot_1_14_4_Compatibility;
 import cz.nerdy.craftchat.nms.Spigot_1_15_2_Compatibility;
+import cz.nerdy.craftchat.nms.Spigot_1_16_1_Compatibility;
 import cz.nerdy.craftchat.objects.ChatGroup;
 import cz.nerdy.craftchat.objects.CraftChatPlayer;
 import net.luckperms.api.LuckPerms;
@@ -99,6 +100,9 @@ public class Main extends JavaPlugin {
                 break;
             case "v1_15_R1":
                 this.pluginCompatibility = new Spigot_1_15_2_Compatibility();
+                break;
+            case "v1_16_R1":
+                this.pluginCompatibility = new Spigot_1_16_1_Compatibility();
                 break;
         }
         return this.pluginCompatibility != null;
